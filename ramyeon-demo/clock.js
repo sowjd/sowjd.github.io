f54a3249416fed20f525bd966536c72ee04293d7
Sojeong Sim <sowjd1225@gmail.com>
2021-03-29 16:51:32 +0900

    Add files via upload

---
+++ ramyeon-demo/clock.js
@@ -0,0 +1,17 @@
+const clock = document.querySelector(".js-clock");
+
+function getTime(){
+  const date = new Date();
+  const hour = date.getHours();
+  const minute = date.getMinutes();
+  const second = date.getSeconds();
+  // clockTitle.innerText = `${hour < 10 ? `0${hour}`:hour}:${minute < 10 ? `0${minute}` : minute}:${second < 10 ? `0${second}` : second}`;
+  clock.innerText = `${hour < 10 ? `0${hour}`:hour}:${minute < 10 ? `0${minute}` : minute}:${second < 10 ? `0${second}` : second}`;
+}
+
+function init(){
+  getTime();
+  setInterval(getTime, 1000);
+}
+
+init();
